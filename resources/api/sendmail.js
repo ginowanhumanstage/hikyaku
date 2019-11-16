5499830f20c350af42a9ca7e1429cdbd772dd0e9
hirofumi_i <detroit69city.nau@gmail.com>
2019-11-16 11:20:07 +0900

    Update sendmail.js

--- resources/api/sendmail.js
+++ resources/api/sendmail.js
@@ -1,6 +1,61 @@
-exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: `Hello, World`
+const sendMail = require("sendmail")();
+const { validateEmail, validateLength } = require("../../utils/validations");
+
+exports.handler = (event, context, callback) => {
+  if (!process.env.CONTACT_EMAIL) {
+    return callback(null, {
+      statusCode: 500,
+      body: "process.env.CONTACT_EMAIL must be defined"
+    });
+  }
+
+  const body = JSON.parse(event.body);
+
+  try {
+    validateLength("body.name", body.name, 3, 50);
+  } catch (e) {
+    return callback(null, {
+      statusCode: 403,
+      body: e.message
+    });
+  }
+
+  try {
+    validateEmail("body.email", body.email);
+  } catch (e) {
+    return callback(null, {
+      statusCode: 403,
+      body: e.message
+    });
+  }
+
+  try {
+    validateLength("body.details", body.details, 10, 1000);
+  } catch (e) {
+    return callback(null, {
+      statusCode: 403,
+      body: e.message
+    });
+  }
+
+  const descriptor = {
+    from: `"${body.email}" <no-reply@humanstage.net>`,
+    to: process.env.CONTACT_EMAIL,
+    subject: `${body.name} sent you a message from humanstage.net`,
+    text: body.details
+  };
+
+  sendMail(descriptor, e => {
+    if (e) {
+      callback(null, {
+        statusCode: 500,
+        body: e.message
+      });
+    } else {
+      callback(null, {
+        statusCode: 200,
+        body: ""
+      });
+    }
   });
 };
